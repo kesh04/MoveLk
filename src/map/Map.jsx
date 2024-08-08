f1b753b67afe01cd18de5875bcab1cadb16bd174
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2024-08-08 22:46:02 +0530

    new-ch

--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -1,7 +1,12 @@
+
+
+
+
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from 'expo-location';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const Map = () => {
   const [region, setRegion] = useState(null);
@@ -26,14 +31,33 @@ const Map = () => {
 
   return (
     <View style={styles.container}>
+      <GooglePlacesAutocomplete
+        placeholder='Search'
+        fetchDetails={true}
+        onPress={(data, details = null) => {
+          setRegion({
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+        }}
+        query={{
+          key: 'env.GOOGLE_MAP',
+          language: 'en',
+        }}
+        styles={{
+          container: { flex: 0 },
+          listView: { backgroundColor: 'white' },
+        }}
+      />
       {region && (
         <MapView
           style={styles.map}
           initialRegion={region}
           showsUserLocation={true}
         >
-       
-          
+          <Marker coordinate={region} />
         </MapView>
       )}
     </View>
